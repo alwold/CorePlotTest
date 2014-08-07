9fc78d9390535db988de007dab91e6207c07fc1d
Al Wold <alwold@alwold.com>
2014-08-06 22:42:29 -0700

    add bridging header

---
+++ CorePlotTest/CorePlotTest-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  CorePlotTest-Bridging-Header.h
+//  CorePlotTest
+//
+//  Created by Al Wold on 8/6/14.
+//  Copyright (c) 2014 Al Wold. All rights reserved.
+//
+
+#import "CorePlot-CocoaTouch.h"
