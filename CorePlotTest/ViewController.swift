3bfaacc036a47f77a0824c1bd0b09aa8ad1f0be4
Al Wold <alwold@alwold.com>
2014-08-06 22:42:52 -0700

    add graph

--- CorePlotTest/ViewController.swift
+++ CorePlotTest/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, CPTPlotDataSource {
+    @IBOutlet weak var graphView: CPTGraphHostingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // create graph
+        var graph = CPTXYGraph(frame: CGRectZero)
+        graph.title = "Hello Graph"
+        graph.paddingLeft = 0
+        graph.paddingTop = 0
+        graph.paddingRight = 0
+        graph.paddingBottom = 0
+        // hide the axes
+        var axes = graph.axisSet as CPTXYAxisSet
+        var lineStyle = CPTMutableLineStyle()
+        lineStyle.lineWidth = 0
+        axes.xAxis.axisLineStyle = lineStyle
+        axes.yAxis.axisLineStyle = lineStyle
+        
+        // add a pie plot
+        var pie = CPTPieChart()
+        pie.dataSource = self
+        pie.pieRadius = (self.view.frame.size.width * 0.9)/2
+        graph.addPlot(pie)
+        
+        self.graphView.hostedGraph = graph
     }
-
+    
+    func numberOfRecordsForPlot(plot: CPTPlot!) -> UInt {
+        return 4
+    }
+    
+    func numberForPlot(plot: CPTPlot!, field fieldEnum: UInt, recordIndex idx: UInt) -> NSNumber! {
+        return idx+1
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
