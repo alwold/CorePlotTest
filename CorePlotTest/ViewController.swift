b3153c57600ba52b0b28e8012f149647e26e7cf8
Al Wold <alwold@alwold.com>
2014-08-29 18:03:40 -0700

    add really basic scatter plot

--- CorePlotTest/ViewController.swift
+++ CorePlotTest/ViewController.swift
@@ -17,23 +17,11 @@ class ViewController: UIViewController, CPTPlotDataSource {
         // create graph
         var graph = CPTXYGraph(frame: CGRectZero)
         graph.title = "Hello Graph"
-        graph.paddingLeft = 0
-        graph.paddingTop = 0
-        graph.paddingRight = 0
-        graph.paddingBottom = 0
-        // hide the axes
-        var axes = graph.axisSet as CPTXYAxisSet
-        var lineStyle = CPTMutableLineStyle()
-        lineStyle.lineWidth = 0
-        axes.xAxis.axisLineStyle = lineStyle
-        axes.yAxis.axisLineStyle = lineStyle
         
-        // add a pie plot
-        var pie = CPTPieChart()
-        pie.dataSource = self
-        pie.pieRadius = (self.view.frame.size.width * 0.9)/2
-        graph.addPlot(pie)
-        
+        var plot = CPTScatterPlot()
+        plot.dataSource = self
+        graph.addPlot(plot)
+
         self.graphView.hostedGraph = graph
     }
     
